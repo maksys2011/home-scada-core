6704799145aa4288f79e4bde17caa45cf6a8f44c
maksys <maksys2011@yandex.ru>
2025-12-04 21:56:45 +0300

    Отладка конфигураций из файла json

--- app/main.cpp
+++ app/main.cpp
@@ -7,22 +7,8 @@ using json = nlohmann::json;
 
 int main()
 {
-    std::cout << "hello world++++" << std::endl;
-
-    SensorConfig sensor(
-        "sensor_1",
-        "Temperature Sensor",
-        Type::Analog,
-        75.0,
-        65.0,
-        85.0,
-        55.0
-    );
-
+    SensorConfig sensor("config.json");
     sensor.print();
-
-    std::cout << "hello ubuntu" << std::endl;
-
 
 
     return 0;
